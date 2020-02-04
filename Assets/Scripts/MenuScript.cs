5574b79e5d8f4d72bb5f238f9ae91aeb81af474e
acourtin <acourtin@student.42.fr>
2020-02-04 13:29:02 +0100

    finished basic menu functions

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -2,6 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Difficulty{Easy, Normal, Hard};
+
 public class MenuScript : MonoBehaviour
 {
+	public Difficulty difficulty;
+
+	private Animator menuAnimator;
+
+	void Start() {
+		difficulty = Difficulty.Easy;
+		menuAnimator = GetComponent<Animator>();
+	}
+
+	public void goToStageSelection() {
+		menuAnimator.Play("MainMenuToStageSelection", 0, 0f);
+	}
+
+	public void goToMainMenu() {
+		menuAnimator.Play("StageSelectionToMainMenu", 0, 0f);
+	}
 }
--- Assets/Scripts/MouseOverScript.cs
+++ Assets/Scripts/MouseOverScript.cs
@@ -4,40 +4,131 @@ using UnityEngine;
 
 public class MouseOverScript : MonoBehaviour
 {
-	public enum MenuButtonType{Normal, Test, Teapot, Globe, Fortytwo, Elephant, EasyDifficulty, NormalDifficulty, HardDifficulty};
+	public enum MenuButtonType{Normal, Test, Teapot, Globe, Fortytwo, Elephant, EasyDifficulty, NormalDifficulty, HardDifficulty, Back};
 	public MenuButtonType buttonType;
 
+	private MenuScript menuScript;
+	private TextMesh textMesh;
 	private bool isMouseOver = false;
 
+	void Start() {
+		menuScript = GameObject.Find("Menu").GetComponent<MenuScript>();
+		textMesh = GetComponent<TextMesh>();
+	}
+
 	void Update() {
-		if (Input.GetMouseButtonDown(0) && isMouseOver) {
-			if (buttonType == MenuButtonType.Normal) {
-				Debug.Log("Launch Normal Mode");
-			}
-			else if (buttonType == MenuButtonType.Teapot) {
-				Debug.Log("Launch Teapot Scene");
-			}
-			else if (buttonType == MenuButtonType.Globe) {
-				Debug.Log("Launch Globe Scene");
-			}
-			else if (buttonType == MenuButtonType.Fortytwo) {
-				Debug.Log("Launch Fortytwo Scene");
-			}
-			else if (buttonType == MenuButtonType.Elephant) {
-				Debug.Log("Launch Elephant Scene");
-			}
-			else if (buttonType == MenuButtonType.EasyDifficulty) {
-				Debug.Log("Set difficulty to easy");
-			}
-			else if (buttonType == MenuButtonType.NormalDifficulty) {
-				Debug.Log("Set difficulty to normal");
-			}
-			else if (buttonType == MenuButtonType.HardDifficulty) {
-				Debug.Log("Set difficulty to hard");
-			}
-			else {
-				Debug.Log("Launch Test Mode");
-			}
+		// TEXT MESH COLORS
+		switch(buttonType) {
+			case MenuButtonType.Normal:
+				if (isMouseOver) {
+					textMesh.color = new Color(.9f, .9f, .9f);
+					if (Input.GetMouseButtonDown(0)) {
+						Debug.Log("Launch Normal Mode");
+						menuScript.goToStageSelection();
+					}
+				} else {
+					textMesh.color = new Color(.1f, .1f, .1f);
+				}
+				break;
+
+			case MenuButtonType.Teapot:
+				if (isMouseOver) {
+					textMesh.color = new Color(.9f, .9f, .9f);
+					if (Input.GetMouseButtonDown(0)) {
+						Debug.Log("Launch Teapot Scene");
+					}
+				} else {
+					textMesh.color = new Color(.1f, .1f, .1f);
+				}
+				break;
+
+			case MenuButtonType.Globe:
+				if (isMouseOver) {
+					textMesh.color = new Color(.9f, .9f, .9f);
+					if (Input.GetMouseButtonDown(0)) {
+						Debug.Log("Launch Globe Scene");
+					}
+				} else {
+					textMesh.color = new Color(.1f, .1f, .1f);
+				}
+				break;
+
+			case MenuButtonType.Fortytwo:
+				if (isMouseOver) {
+					textMesh.color = new Color(.9f, .9f, .9f);
+					if (Input.GetMouseButtonDown(0)) {
+						Debug.Log("Launch Fortytwo Scene");
+					}
+				} else {
+					textMesh.color = new Color(.1f, .1f, .1f);
+				}
+				break;
+
+			case MenuButtonType.Elephant:
+				if (isMouseOver) {
+					textMesh.color = new Color(.9f, .9f, .9f);
+					if (Input.GetMouseButtonDown(0)) {
+						Debug.Log("Launch Elephant Scene");
+					}
+				} else {
+					textMesh.color = new Color(.1f, .1f, .1f);
+				}
+				break;
+
+			case MenuButtonType.EasyDifficulty:
+				if (menuScript.difficulty == Difficulty.Easy) {
+					textMesh.color = new Color(.9f, .9f, .9f);
+				} else {
+					textMesh.color = new Color(.1f, .1f, .1f);
+				}
+				if (Input.GetMouseButtonDown(0) && isMouseOver) {
+					menuScript.difficulty = Difficulty.Easy;
+				}
+				break;
+
+			case MenuButtonType.NormalDifficulty:
+				if (menuScript.difficulty == Difficulty.Normal) {
+					textMesh.color = new Color(.9f, .9f, .9f);
+				} else {
+					textMesh.color = new Color(.1f, .1f, .1f);
+				}
+				if (Input.GetMouseButtonDown(0) && isMouseOver) {
+					menuScript.difficulty = Difficulty.Normal;
+				}
+				break;
+
+			case MenuButtonType.HardDifficulty:
+				if (menuScript.difficulty == Difficulty.Hard) {
+					textMesh.color = new Color(.9f, .9f, .9f);
+				} else {
+					textMesh.color = new Color(.1f, .1f, .1f);
+				}
+				if (Input.GetMouseButtonDown(0) && isMouseOver) {
+					menuScript.difficulty = Difficulty.Hard;
+				}
+				break;
+
+			case MenuButtonType.Back:
+				if (isMouseOver) {
+					textMesh.color = new Color(.9f, .9f, .9f);
+					if (Input.GetMouseButtonDown(0)) {
+						menuScript.goToMainMenu();
+					}
+				} else {
+					textMesh.color = new Color(.1f, .1f, .1f);
+				}
+				break;
+			default:
+				if (isMouseOver) {
+					textMesh.color = new Color(.9f, .9f, .9f);
+					if (Input.GetMouseButtonDown(0)) {
+						Debug.Log("Launch Test Mode");
+						menuScript.goToStageSelection();
+					}
+				} else {
+					textMesh.color = new Color(.1f, .1f, .1f);
+				}
+				break;
 		}
 	}
 
